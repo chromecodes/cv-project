032dfa8f2c84ea0d88e01f5279133cc7c7cba115
chromecodes <hameedhussain782@gmail.com>
2022-10-28 21:35:17 +0530

    updated file

---
+++ src/components/Bio.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class Bio extends Component {
+  render() {
+    return (
+      <div>
+        <div className='name'> 
+            <input type="text" placeholder='First Name'/>
+            <input type="text" placeholder='Last Name'/>
+            <input type="text" placeholder='Title '/>
+        </div>
+        <div className='contacts '> 
+            <input type="email" placeholder='Email'/>
+            <input type="phone" placeholder='Phone'/>
+        </div>
+        <div className='links '> 
+            <input type="text" placeholder='LinkedIn'/>
+            <input type="text" placeholder='GitHub'/>
+        </div>
+        <div className='desp'> 
+            <textarea className='about'  placeholder="About yourself" maxLength="150"></textarea>
+        </div>
+        <div>
+          <button className='nxt btn'> Next </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bio
