0db1faebe62fc0cb4f2840a34e51632421d3f69c
chromecodes <hameedhussain782@gmail.com>
2022-10-30 21:17:49 +0530

    experiments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Bio from "./components/Bio";
 import Educ from "./components/Educ";
+import Exp from "./components/Exp";
+import Preview from "./components/Preview";
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
   return (
-    <div>hello worl
-      <Bio />
-      <Educ/>
+    <div>
+      <Preview />
 
 
+  
     </div>
    );
 }
