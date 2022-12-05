6499a448f4fb4e2bb408486500e5dfe04ae3a19b
chromecodes <hameedhussain782@gmail.com>
2022-12-06 00:24:57 +0530

    Updates

---
+++ precache-manifest.ca00a2ae33bf2186658dbf7a06c7a10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a0c0fd276836b39916",
+    "url": "/static/css/main.5e842336.chunk.css"
+  },
+  {
+    "revision": "15a0c0fd276836b39916",
+    "url": "/static/js/main.0255d611.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b71464ac81d110346b4",
+    "url": "/static/js/2.3dcae9da.chunk.js"
+  },
+  {
+    "revision": "59adfa082df311384e1d7b4bb3e7ec9c",
+    "url": "/index.html"
+  }
+];
